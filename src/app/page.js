39fdf52a9098c238569ef1560697e1baf3aa07bd
AlFahadMunna <afmunna.afm.afm@gmail.com>
2026-05-08 12:29:43 +0600

    marquee section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "@/components/Banner";
+import MarqueeSection from "@/components/Marquee";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <MarqueeSection />
     </div>
   );
 }
---
+++ src/components/Marquee.jsx
@@ -0,0 +1,14 @@
+import Marquee from "react-fast-marquee";
+
+const MarqueeSection = () => {
+  return (
+    <div className="bg-primary h-20 container mx-auto border border-black rounded flex items-center text-xl font-semibold text-white ">
+      <Marquee pauseOnHover className="h-full">
+        New Arrivals: Whispers of the Ocean | Next.js in Action | Quantum World
+        | The Midnight Letter | Special Discount on Memberships..
+      </Marquee>
+    </div>
+  );
+};
+
+export default MarqueeSection;
